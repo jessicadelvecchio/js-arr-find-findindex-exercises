d6f5176c969599f0f0da46125075b123794f52fb
Jessica DelVecchio <hey@jdvdv.com>
2024-10-14 19:09:37 -0700

    exercises completet

--- find-findindex.js
+++ find-findindex.js
@@ -11,8 +11,18 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+// ? would this not work? is there a way to make this work?
+// function findUserByUsername(usersArray, username) {
+//   return usersArray.find(function (val, key) {
+//     return val[key] === username;
+//   });
+// }
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function (user) {
+    return user.username === username;
+  });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +37,22 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+const users = [
+  { username: "mlewis" },
+  { username: "akagen" },
+  { username: "msmith" },
+];
+
+function removeUser(usersArray, username) {
+  // find index of username
+  const rmIdx = usersArray.findIndex(function (user) {
+    return user.username === username;
+  });
+
+  // if username does not exist return undefined
+  if (rmIdx === -1) return;
+
+  //remove username from the users array
+  // return username removed
+  return usersArray.splice(rmIdx, 1)[0];
+}
